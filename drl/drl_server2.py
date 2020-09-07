af41d2bc089ec96b7fb9456d38da8d27d90377be
mario-silva-duarte <msduarte@yahoo.com>
2020-09-07 22:07:41 +0100

    Debug

--- drl/drl_server2.py
+++ drl/drl_server2.py
@@ -124,7 +124,7 @@ class ParametricActionsModel(DistributionalQTFModel):
     def forward(self, input_dict, state, seq_lens):
 
         print("{} : [INFO] ParametricActionsModel Input_Dict['obs']['cart'] {}"
-             .format(datetime.now(),input_dict["obs"]['cart']))
+             .format(datetime.now(),tf.concat(input_dict["obs"]['cart'],1)))
         # Extract the available actions tensor from the observation.
         action_mask = input_dict["obs"]["action_mask"]
 
