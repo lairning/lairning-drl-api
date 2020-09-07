bef737ff19b5e32b024401edccde93ad81ad8c1d
mario-silva-duarte <msduarte@yahoo.com>
2020-09-07 21:49:21 +0100

    Debug

--- drl/drl_server2.py
+++ drl/drl_server2.py
@@ -110,7 +110,7 @@ class ParametricActionsModel(DistributionalQTFModel):
         print("{} : [DEBUG] ParametricActionsModel ActS={}, ObsS={}, NOut={}, Name={}"
              .format(datetime.now(),action_space, obs_space, num_outputs, name))
 
-        model_observation_space = Box(low=0, high=1, shape=(obs_space-action_space.n,))
+        model_observation_space = Box(low=0, high=1, shape=(obs_space.shape[0]-action_space.n,))
 
         print("{} : [DEBUG] ParametricActionsModel {}"
              .format(datetime.now(),action_space, obs_space, num_outputs, name))
