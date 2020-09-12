869624bdb7a686f1ec2f32987ebe7d9a49153305
mario-silva-duarte <msduarte@yahoo.com>
2020-09-12 17:18:50 +0100

    Testing

--- drl_client_parametric.py
+++ drl_client_parametric.py
@@ -291,7 +291,7 @@ if __name__ == "__main__":
 
     # Run DQN and PPO Models
 
-    for model in ['APPO','Apex', 'SimpleQ']:
+    for model in ['Apex', 'SimpleQ']:
 
         start_msg = {'action_space_size': max_action_size,
                      'observation_space_size': flat_observation_space_size,
