e39b426ea0ad90c06c8196be13e91e0ed95911ca
mario-silva-duarte <msduarte@yahoo.com>
2020-12-15 20:01:17 +0000

    Debug

--- drl/drl_server2.py
+++ drl/drl_server2.py
@@ -221,8 +221,8 @@ class DRLServer:
                   .format(datetime.now(), err))
             return {'status': False, 'error': err}
 
-        #print("{} : DEBUG DRLServer Payload {}"
-        #      .format(datetime.now(), payload))
+        print("{} : DEBUG DRLServer observation_space {}"
+              .format(datetime.now(), payload))
 
         drl_trainer_args = (
             trainer_log_file,
